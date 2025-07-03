75f3e69bb770fa535426e571114dfebfa9c6fb55
moxuy <jichangee@gmail.com>
2025-07-03 17:07:15 +0800

    feat: 添加配置

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@ export const config = {
   title: "node",
   bossContent: ` @ multi (webpack)-dev-server/client?http://192.168.0.189:8081/sockjs-node (webpack)/hot/dev-server.js ./src/main.js
 
-    warning  in ./src/pagesActive/slotMachine/index5.vue?vue&type=template&id=6430da5c&scoped=true&
+    warning  in ./src/pages/reader/index.vue?vue&type=template&id=6430da5c&scoped=true&
 
     Module Warning (from ./node_modules/@dcloudio/vue-cli-plugin-uni/packages/vue-loader/lib/loaders/templateLoader.js):
     (Emitted value instead of an instance of Error) <v-uni-view v-for="label in item.label">: component lists rendered with v-for should have explicit keys. See https://vuejs.org/guide/list.html#key for more info.
 
-    @ ./src/pagesActive/slotMachine/index5.vue?vue&type=template&id=6430da5c&scoped=true& 1:0-701 1:0-701
-    @ ./src/pagesActive/slotMachine/index5.vue
+    @ ./src/pages/reader/index.vue?vue&type=template&id=6430da5c&scoped=true& 1:0-701 1:0-701
+    @ ./src/pages/reader/index.vue
     @ ./src/pages.json
     @ ./src/main.js
     @ multi (webpack)-dev-server/client?http://192.168.0.189:8081/sockjs-node (webpack)/hot/dev-server.js ./src/main.js
